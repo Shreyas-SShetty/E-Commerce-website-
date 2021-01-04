2e2204a03077719625f9076e96ebad65a054835c
shreyas_shetty <shreyasshetty788@gmail.com>
2021-01-04 08:45:40 +0530

    Add new Item

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home_page, name='home_page'),
     path('item/<int:pk>/', views.item_page, name='item_detail'),
+    path('item/new/', views.item_new, name='item_new'),
 ]
